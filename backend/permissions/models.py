8565a4472eb5d542d5caf962cc0cca72a40b42ae
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-09-01 23:56:00 +0500

    Changes

--- backend/permissions/models.py
+++ backend/permissions/models.py
@@ -8,10 +8,12 @@ class Role(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     permissions = models.ManyToManyField('Permission', related_name='roles')
-
+    code = models.CharField(max_length=50, null=True, blank=True)
+    
 
 class Permission(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     code = models.CharField(max_length=50)
-
+    module_name = models.CharField(max_length=50, null=True, blank=True)
+    
