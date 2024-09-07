9622789443a98861744e6b90e582ecd71865eed5
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-09-07 18:21:17 +0500

    Changes

--- backend/hrm/settings.py
+++ backend/hrm/settings.py
@@ -187,13 +187,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# myproject/settings.py
+# # myproject/settings.py
 
-# Redis configuration for Celery broker
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# # Redis configuration for Celery broker
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
-# Celery settings
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_TIMEZONE = 'UTC'
+# # Celery settings
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_TIMEZONE = 'UTC'
